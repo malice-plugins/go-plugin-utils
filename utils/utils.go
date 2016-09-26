5e76e391af451c9268cc6d52a6a8644379551f28
blacktop <blacktop@users.noreply.github.com>
2016-09-25 20:58:35 -0600

    add stringInSlice

--- utils/utils.go
+++ utils/utils.go
@@ -98,3 +98,13 @@ func GetHashType(hash string) (string, error) {
 		return "", errors.New("This is not a valid hash.")
 	}
 }
+
+// StringInSlice returns whether or not a string exists in a slice
+func StringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
