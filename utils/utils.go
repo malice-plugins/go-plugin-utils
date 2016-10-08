fc02a088f23883fd512651680a8017379d7b67b5
blacktop <blacktop@users.noreply.github.com>
2016-10-08 13:58:21 -0600

    well that was dumb

--- utils/utils.go
+++ utils/utils.go
@@ -16,26 +16,6 @@ import (
 
 var camelingRegex = regexp.MustCompile("[0-9A-Za-z]+")
 
-// RemoveDuplicates removes duplicates from a string array
-func RemoveDuplicates(elements []string) []string {
-	// Use map to record duplicates as we find them.
-	encountered := map[string]bool{}
-	result := []string{}
-
-	for v := range elements {
-		if encountered[elements[v]] == true {
-			// Do not add duplicate.
-		} else {
-			// Record this element as an encountered element.
-			encountered[elements[v]] = true
-			// Append to result slice.
-			result = append(result, elements[v])
-		}
-	}
-	// Return the new slice.
-	return result
-}
-
 // CamelCase converts strings to their camel case equivalent
 func CamelCase(src string) string {
 	byteSrc := []byte(src)
