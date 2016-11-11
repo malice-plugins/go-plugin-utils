a5946b0d05f7c2898cb63b73531a906f08aa45a7
blacktop <blacktop@users.noreply.github.com>
2016-11-11 12:13:39 -0700

    add alpine golang upgrade script

---
+++ scripts/upgrade-alpine-go.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+GOLANG_VERSION=1.7.3
+GOLANG_SRC_URL=https://golang.org/dl/go$GOLANG_VERSION.src.tar.gz
+GOLANG_SRC_SHA256=79430a0027a09b0b3ad57e214c4c1acfdd7af290961dd08d322818895af1ef44
+
+COPY no-pic.patch /
+
+echo "Upgrade to Golang $GOLANG_VERSION..."
+
+export GOROOT_BOOTSTRAP="$(go env GOROOT)"
+
+wget -q "$GOLANG_SRC_URL" -O /no-pic.patch
+wget -q "$GOLANG_SRC_URL" -O golang.tar.gz
+echo "$GOLANG_SRC_SHA256  golang.tar.gz" | sha256sum -c -
+
+tar -C /usr/local -xzf golang.tar.gz
+rm golang.tar.gz
+
+cd /usr/local/go/src
+
+patch -p2 -i /no-pic.patch
+./make.bash
+rm -rf /*.patch
+
+export GOPATH=/go
+export PATH=$GOPATH/bin:/usr/local/go/bin:$PATH
