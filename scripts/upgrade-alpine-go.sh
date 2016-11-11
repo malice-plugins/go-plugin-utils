0f5af4532cebd3a6c64552676ce3a04dad1fbc18
blacktop <blacktop@users.noreply.github.com>
2016-11-11 12:15:10 -0700

    update script

--- scripts/upgrade-alpine-go.sh
+++ scripts/upgrade-alpine-go.sh
@@ -5,14 +5,14 @@ set -e
 GOLANG_VERSION=1.7.3
 GOLANG_SRC_URL=https://golang.org/dl/go$GOLANG_VERSION.src.tar.gz
 GOLANG_SRC_SHA256=79430a0027a09b0b3ad57e214c4c1acfdd7af290961dd08d322818895af1ef44
-
+PATCH_URL=https://raw.githubusercontent.com/maliceio/go-plugin-utils/master/scripts/no-pic.patch
 COPY no-pic.patch /
 
 echo "Upgrade to Golang $GOLANG_VERSION..."
 
 export GOROOT_BOOTSTRAP="$(go env GOROOT)"
 
-wget -q "$GOLANG_SRC_URL" -O /no-pic.patch
+wget -q "$PATCH_URL" -O /no-pic.patch
 wget -q "$GOLANG_SRC_URL" -O golang.tar.gz
 echo "$GOLANG_SRC_SHA256  golang.tar.gz" | sha256sum -c -
 
